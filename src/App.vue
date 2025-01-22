8c78127cbc826267bbef9bd04dea9da5fd985b06
Abel Barrera <abelbb95@gmail.com>
2025-01-22 23:14:39 +0100

    Added pizza base

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
 <script setup>
+import PizzaMaker from './components/PizzaMaker.vue'
 </script>
 
-<template>
+<template lang="pug">
+  h1 Pizza Maker
+  p Select the toppings by typping them.
+
+  PizzaMaker
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+h1 {
+  font-size: 2.5rem;
+  color: #dea324;
+  font-weight: bold;
+  /* Border effect using text-shadow */
+  text-shadow:
+    -1px -1px 0 #000000,
+    1px -1px 0 #000000,
+    -1px 1px 0 #000000,
+    1px 1px 0 #000000;
+}
+
+p {
+  font-size: 1.2rem;
+  color: #ffffff;
+  line-height: 1.6;
+  /* Border effect using text-shadow */
+  text-shadow:
+    -1px -1px 0 #000000,
+    1px -1px 0 #000000,
+    -1px 1px 0 #000000,
+    1px 1px 0 #000000;
+}
+</style>
---
+++ src/components/PizzaMaker.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  .pizza
+    .pizza__base
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+.pizza {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+  font-family: Arial, sans-serif;
+
+  /* Pizza Base with Image */
+  &__base {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    position: relative;
+    margin-bottom: 20px;
+    background: url('@/assets/pizza.webp') no-repeat center center;
+    background-size: cover;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+    overflow: hidden;
+  }
+}
+</style>
